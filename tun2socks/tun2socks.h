9767590f450ac6d47ddbaae1ede52520c2b3335d
Zhuhao Wang <zhuhaow@gmail.com>
2016-05-19 18:26:42 +0800

    Initial Commit

---
+++ tun2socks/tun2socks.h
@@ -0,0 +1,19 @@
+//
+//  tun2socks.h
+//  tun2socks
+//
+//  Created by Zhuhao Wang on 5/19/16.
+//  Copyright © 2016 Zhuhao Wang. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for tun2socks.
+FOUNDATION_EXPORT double tun2socksVersionNumber;
+
+//! Project version string for tun2socks.
+FOUNDATION_EXPORT const unsigned char tun2socksVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <tun2socks/PublicHeader.h>
+
+
