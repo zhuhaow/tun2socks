ff5ad302dbe63ad0eefa73be87a1cf2a6d74bf92
Zhuhao Wang <zhuhaow@gmail.com>
2016-06-15 13:50:35 +0800

    Update build doc script

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
-git checkout gh-pages
-git submodule update --remote
-jazzy
+# only build doc for master branch
+export SOURCE_BRANCH="master"
+export DOC_BRANCH="gh-pages"
+
+# this is the script actually build docs
+./scripts/build_docs.sh
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    echo "Skipping deploy; just doing a build."
+    exit 0
+fi
+
+# upload docs
+./scripts/push_docs.sh
