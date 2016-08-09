be6ccb818f576ce1477b6c49591979f9f620d02e
Zhuhao Wang <zhuhaow@gmail.com>
2016-08-09 16:04:06 +0800

    Update doc building system

---
+++ docs/build_docs.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env sh
+
+# only build doc for master branch
+export SOURCE_BRANCH="master"
+export DOC_BRANCH="gh-pages"
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    exit 0
+fi
+
+
+# build docs now
+gem install jazzy --no-ri --no-rdoc
+jazzy --config docs/.jazzy.yaml
+
+# remove all redundant files
+find . -not -name "docs" -not -name ".git" -maxdepth 1 -mindepth 1 -print0 | xargs -0 rm -rf --
+# copy docs files to root
+cp -r docs/. .
+rm -rf docs
+
+# push update
+git checkout --orphan gh-pages
+git add .
+git -c user.name="Travis CI" commit -m "Update docs"
+git push --force --quiet https://$GITHUB_API_KEY@github.com/zhuhaow/tun2socks.git gh-pages > /dev/null 2>&1
