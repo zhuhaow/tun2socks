f15d9f4e7c9a944d118276534f6d0fde2fdaa8c2
Zhuhao Wang <zhuhaow@gmail.com>
2016-11-25 22:25:49 +0800

    Set up CircleCI

--- docs/build_docs.sh
+++ docs/build_docs.sh
@@ -4,17 +4,17 @@
 export SOURCE_BRANCH="master"
 export DOC_BRANCH="gh-pages"
 
-if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+if [ "$CI_PULL_REQUEST" != "" -o "$CIRCLE_BRANCH" != "$SOURCE_BRANCH" ]; then
     exit 0
 fi
 
 
 # build docs now
-gem install jazzy --no-ri --no-rdoc
-jazzy --config docs/.jazzy.yaml
+bundle update
+bundle exec jazzy --config docs/.jazzy.yaml
 
 # remove all redundant files
-find . -not -name "docs" -not -name ".git" -maxdepth 1 -mindepth 1 -print0 | xargs -0 rm -rf --
+find . -not -name "docs" -not -name ".git" -maxdepth 1 -print0 | xargs -0 rm -rf --
 # copy docs files to root
 cp -r docs/. .
 rm -rf docs
@@ -22,5 +22,5 @@ rm -rf docs
 # push update
 git checkout --orphan gh-pages
 git add .
-git -c user.name="Travis CI" commit -m "Update docs"
-git push --force --quiet https://$GITHUB_API_KEY@github.com/zhuhaow/tun2socks.git gh-pages > /dev/null 2>&1
+git -c user.name="Circle CI" commit -m "Update docs [skip ci]"
+git push --force --quiet https://$GITHUB_TOKEN@github.com/zhuhaow/tun2socks.git gh-pages > /dev/null 2>&1
